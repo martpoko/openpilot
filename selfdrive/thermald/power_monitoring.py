f452d189cc439205189ae00bd98b4d366bd95a16
Martin Pokorny <32168743+martpoko@users.noreply.github.com>
2021-01-12 18:51:33 -0500

    Tweaked auto shutdown parameters

--- selfdrive/thermald/power_monitoring.py
+++ selfdrive/thermald/power_monitoring.py
@@ -17,8 +17,8 @@ CAR_VOLTAGE_LOW_PASS_K = 0.091 # LPF gain for 5s tau (dt/tau / (dt/tau + 1))
 CAR_BATTERY_CAPACITY_uWh = 30e6
 CAR_CHARGING_RATE_W = 45
 
-VBATT_PAUSE_CHARGING = 11.0
-MAX_TIME_OFFROAD_S = 4*3600
+VBATT_PAUSE_CHARGING = 11.7 # Minimum voltage, cutoff point for C2
+MAX_TIME_OFFROAD_S = 6*3600 # Time C2 will stay alive after drive
 
 # Parameters
 def get_battery_capacity():
